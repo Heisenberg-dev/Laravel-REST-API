4ae37113b0a0ac392d8b05aef9b0a4ddc2e2dc59
Heisenberg-dev <arthur.bagrov@gmail.com>
2024-10-10 21:56:11 +0300

    filtering data

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -2,35 +2,36 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use Illuminate\Http\Request;
+
+
+use App\services\V1\CustomerQuery;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
-use Request;
+
+
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
-
-    {
-        $filter = new CustomerQuery();
-        $queryItems = $filter->transform($request); //['column', 'operator', 'value']
-        Customer::where($queryItems);
-
-        if(count($queryItems) == 0){
-            return new CustomerCollection(Customer::paginate())
-        }else {
-            return new CustomerCollection(Customer::where($queryItems)->paginate());
-        }
-
-
+    public function index(Request $request, CustomerQuery $query)
+{
+    // Используйте $query, который уже передан как зависимость
+    $queryItems = $query->transform($request); //['column', 'operator', 'value']
+    
+    if (count($queryItems) == 0) {
         return new CustomerCollection(Customer::paginate());
+    } else {
+        return new CustomerCollection(Customer::where($queryItems)->paginate());
     }
+}
+
 
     /**
      * Show the form for creating a new resource.
